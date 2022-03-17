e6d05dc5c4947841ea08ee64d6e77c1b7807223f
Bahaa Alhagar <bahaa.rock@gmail.com>
2022-03-17 22:49:15 +0200

    routing

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers'], function () {
+    Route::apiResource('schools', 'SchoolController')
+        ->middleware('auth:api');
+
+    Route::apiResource('students', 'StudentController')
+        ->middleware('auth:api');
 });
